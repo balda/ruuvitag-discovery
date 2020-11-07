f57acf892a776a217abfd1046eb3ef50fccb5576
Balda <xavier@creative-area.net>
2020-11-07 13:27:07 +0100

    Save config (sampling and battery)

--- svelte/Config/Panel.svelte
+++ svelte/Config/Panel.svelte
@@ -7,21 +7,20 @@
     export let root;
     let col_left = 5;
     let col_right = 6;
-    let state = `view`; // `view` | `saving` | `editSampling`
-    let saving = `none`; // `none` | `sampling` | `battery`
-    let stateSampling = `view`; // `view` | `edit` | `save`
+    let state = `view`; // `view` | `saving`
+    let saving = ``;
     let stateConfig = `hidden`; // `hidden` | `export` | `export`
+    let editSampling = false;
+    let editBattery = false;
     function save(target) {
         return async function() {
             state = `saving`;
             saving = target;
             stateConfig = `hidden`;
-            stateSampling = `save`;
             const data = {};
             data[`${target}`] = config[target];
             await post(`${root}config`, data);
             state = `view`;
-            stateSampling = `view`;
         }
     }
 </script>
@@ -32,7 +31,54 @@
             Sampling Configuration
         </small>
         <div class="pl-2">
-            {#if state === `view`}
+            {#if editSampling}
+                <form id="form-sampling" class="mt-4" disabled>
+                    <div class="form-group row">
+                        <label class="col-sm-{col_left} col-form-label-sm">
+                            History
+                        </label>
+                        <div class="col-sm-{col_right}">
+                            <input type="number" name="history"
+                             bind:value="{config.sampling.history}"
+                             class="form-control form-control-sm"
+                             disabled={state === `saving` ? `disabled` : null}
+                            >
+                            <small class="form-text text-muted">
+                                <em>Max samples in history</em>
+                            </small>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-{col_left} col-form-label-sm">
+                            Sampling interval
+                        </label>
+                        <div class="col-sm-{col_right}">
+                            <input type="number" name="interval"
+                             bind:value="{config.sampling.interval}"
+                             class="form-control form-control-sm"
+                             disabled={state === `saving` ? `disabled` : null}
+                            >
+                            <small class="form-text text-muted">
+                                <em>Sampling interval (in ms)</em>
+                            </small>
+                        </div>
+                    </div>
+                </form>
+                {#if state === `saving` && saving === `sampling`}
+                    <div>
+                        Saving ...
+                    </div>
+                {:else}
+                    <a href="/" on:click|preventDefault={() => {editSampling = false}} class="btn btn-light btn-sm mr-4">
+                        Cancel
+                    </a>
+                    <a href="/" on:click|preventDefault={save(`sampling`)}
+                     class="btn btn-light btn-sm {state === `saving` ? `disabled` : null}"
+                    >
+                        Save
+                    </a>
+                {/if}
+            {:else}
                 <div class="small py-1">
                     <div class="mt-2 mb-2">
                         History: {config.sampling.history}
@@ -48,44 +94,9 @@
                     </div>
                 </div>
                 <div>
-                    <a href="/" on:click|preventDefault={() => {state = `editSampling`}} class="btn btn-light btn-sm">
+                    <a href="/" on:click|preventDefault={() => {editSampling = true}} class="btn btn-light btn-sm">
                         Edit
                     </a>
-                </div>
-            {:else if state === `editSampling`}
-                <form id="form-sampling" class="mt-4" disabled>
-                    <div class="form-group row">
-                        <label class="col-sm-{col_left} col-form-label-sm">
-                            History
-                        </label>
-                        <div class="col-sm-{col_right}">
-                            <input type="number" name="history" bind:value="{config.sampling.history}" class="form-control form-control-sm" disabled={stateSampling === `save` ? `disabled` : null}>
-                            <small id="passwordHelpBlock" class="form-text text-muted">
-                                <em>Max samples in history</em>
-                            </small>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-{col_left} col-form-label-sm">
-                            Sampling interval
-                        </label>
-                        <div class="col-sm-{col_right}">
-                            <input type="number" name="history" bind:value="{config.sampling.interval}" class="form-control form-control-sm" disabled={stateSampling === `save` ? `disabled` : null}>
-                            <small id="passwordHelpBlock" class="form-text text-muted">
-                                <em>Sampling interval (in ms)</em>
-                            </small>
-                        </div>
-                    </div>
-                </form>
-                <a href="/" on:click|preventDefault={() => {state = `view`}} class="btn btn-light btn-sm mr-4">
-                    Cancel
-                </a>
-                <a href="/" on:click|preventDefault={save(`sampling`)} class="btn btn-light btn-sm">
-                    Save
-                </a>
-            {:else if saving === `sampling`}
-                <div>
-                    Saving ...
                 </div>
             {/if}
         </div>
@@ -94,48 +105,76 @@
         <small class="px-2 py-1 bg-light">
             Battery Level Configuration
         </small>
-        <form id="form-battery" class="mt-4">
-            <div class="form-group row">
-                <label class="col-sm-{col_left} col-form-label-sm">
-                    Min (1%)
-                    <small class="form-text text-muted">
-                        <em>
-                            <span class="font-weight-lighter">default 2500mV</span>
-                        </em>
-                    </small>
-                </label>
-                <div class="col-sm-{col_right}">
-                    <input type="number" name="history" value="{config.battery.min}" class="form-control form-control-sm">
-                    <small class="form-text text-muted">
-                        <em>
+        <div class="pl-2">
+            {#if editBattery}
+                <form id="form-battery" class="mt-4" disabled>
+                    <div class="form-group row">
+                        <label class="col-sm-{col_left} col-form-label-sm">
+                            Min (1%)
+                        </label>
+                        <div class="col-sm-{col_right}">
+                            <input type="number" name="min"
+                             bind:value="{config.battery.min}"
+                             class="form-control form-control-sm"
+                             disabled={state === `saving` ? `disabled` : null}
+                            >
+                            <small class="form-text text-muted">
+                                <em>Min mV for 1% battery level</em>
+                            </small>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-{col_left} col-form-label-sm">
+                            Max (100%)
+                        </label>
+                        <div class="col-sm-{col_right}">
+                            <input type="number" name="max"
+                             bind:value="{config.battery.max}"
+                             class="form-control form-control-sm"
+                             disabled={state === `saving` ? `disabled` : null}
+                            >
+                            <small class="form-text text-muted">
+                                <em>Max mV for 100% battery level</em>
+                            </small>
+                        </div>
+                    </div>
+                </form>
+                {#if state === `saving` && saving === `battery`}
+                    <div>
+                        Saving ...
+                    </div>
+                {:else}
+                    <a href="/" on:click|preventDefault={() => {editBattery = false}} class="btn btn-light btn-sm mr-4">
+                        Cancel
+                    </a>
+                    <a href="/" on:click|preventDefault={save(`battery`)}
+                     class="btn btn-light btn-sm {state === `saving` ? `disabled` : null}"
+                    >
+                        Save
+                    </a>
+                {/if}
+            {:else}
+                <div class="small py-1">
+                    <div class="mt-2 mb-2">
+                        Min (1%): {config.battery.min}
+                        <div class="font-italic font-weight-lighter">
                             Min mV for 1% battery level
-                        </em>
-                    </small>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-{col_left} col-form-label-sm">
-                    Max (100%)
-                    <small class="form-text text-muted">
-                        <em>
-                            <span class="font-weight-lighter">default 3000mV</span>
-                        </em>
-                    </small>
-                </label>
-                <div class="col-sm-{col_right}">
-                    <input type="number" name="history" value="{config.battery.max}" class="form-control form-control-sm">
-                    <small class="form-text text-muted">
-                        <em>
+                        </div>
+                    </div>
+                    <div class="mb-4">
+                        Max (100%): {config.battery.max}
+                        <div class="font-italic font-weight-lighter">
                             Max mV for 100% battery level
-                        </em>
-                    </small>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </form>
-        <a href="/" on:click|preventDefault={save(`battery`)}
-         class="btn btn-light btn-sm">
-            Save
-        </a>
+                <div>
+                    <a href="/" on:click|preventDefault={() => {editBattery = true}} class="btn btn-light btn-sm">
+                        Edit
+                    </a>
+                </div>
+            {/if}
+        </div>
     </Col>
 </Row>
 <Row class="border-top pt-3 mt-3">
